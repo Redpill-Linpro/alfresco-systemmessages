fb673b6e324bebd658c2c7f15c5d98e6288b605d
Erik Billerby <billerby@gmail.com>
2016-09-09 15:03:00 +0200

    model etc.

---
+++ repo/src/main/java/org/redpill/alfresco/systemmessages/model/SystemMessagesModel.java
@@ -0,0 +1,7 @@
+package org.redpill.alfresco.systemmessages.model;
+
+public interface SystemMessagesModel {
+	public static final String URI = "http://www.redpill-linpro.com/model/sm/1.0";
+	public static final String NS_PREFIX = "rlsm";
+
+}
